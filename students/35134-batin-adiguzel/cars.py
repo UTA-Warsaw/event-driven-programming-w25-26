d65a1e8190c2e97d66c88b0fc95b82feb89d715a
Batin Adiguzel <batinadiguzel7@gmail.com>
2025-12-05 17:04:15 +0100

    Add Car class

---
+++ students/35134-batin-adiguzel/cars.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self, id, brand, model, year):
+        self.id = id
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+car1 = Car(1, "Honda", "Civic Si", 2023)
+car2 = Car(2, "Audi", "RS6", 2016)
