7dacf0719df691e447f566caf8038e84bc346df3
yamozq <ibrahimlikamran1990@gmail.com>
2026-01-28 11:46:05 +0000

    1on1 update

--- 1on1.py
+++ 1on1.py
@@ -1,1 +1,11 @@
 print("Kamran")
+class Student:
+    def __init__(self,name,index_number):
+        self.name = name
+        self.index = index_number
+Student1=Student("Kamran",35315)
+print(Student1.name)
+
+
+
+        
